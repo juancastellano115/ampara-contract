2523b649b1496c17098e5df936b9e74ea7416e5d
Juan Castellano <juancastellano115@gmail.com>
2022-03-28 20:33:28 +0200

    Initial commit

---
+++ contracts/CoffeeMachine.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract CoffeeMachine {
+    uint256 totalCoffee;
+
+    address payable public owner; 
+
+    /*
+     * A little magic, Google what events are in Solidity!
+     */
+    event NewCoffee(
+        address indexed from,
+        uint256 timestamp,
+        string name
+    );
+
+    constructor() payable {
+        console.log("Smart Contract Initialized");
+
+        // user who is calling this function address
+        owner = payable(msg.sender);
+    }
+
+    /*
+     * I created a struct here named Coffee.
+     * A struct is basically a custom datatype where we can customize what we want to hold inside it.
+     */
+    struct Coffee {
+        address giver; // The address of the user who buys me a coffee.
+        string name; // The name of the user who buys me a coffee.
+        uint256 timestamp; // The timestamp when the user buys me a coffee.
+    }
+
+    /*
+     * I declare variable coffee that lets me store an array of structs.
+     * This is what lets me hold all the coffee anyone ever sends to me!
+     */
+    Coffee[] coffee;
+
+    /*
+     * I added a function getAllCoffee which will return the struct array, coffee, to us.
+     * This will make it easy to retrieve the coffee from our website!
+     */
+    function getAllCoffee() public view returns (Coffee[] memory) {
+        return coffee;
+    }
+
+    // Get All coffee bought
+    function getTotalCoffee() public view returns (uint256) {
+        // Optional: Add this line if you want to see the contract print the value!
+        // We'll also print it over in run.js as well.
+        console.log("We have %d total coffee recieved ", totalCoffee);
+        return totalCoffee;
+    }
+
+    function buyCoffee(
+        string memory _name
+    ) public payable {
+        uint256 cost = 0.001 ether;
+        uint256 dexBalance = address(this).balance;
+        require(msg.value >= cost, "Insufficient Ether provided");
+        require(msg.value <= dexBalance, "Not enough tokens in the reserve");
+
+        totalCoffee += 1;
+        console.log("%s has just sent a coffee!", msg.sender);
+
+        /*
+         * This is where I actually store the coffee data in the array.
+         */
+        coffee.push(Coffee(msg.sender, _name, block.timestamp));
+
+        (bool success ) = owner.send(msg.value);
+        require(success, "Failed to send money");
+
+        emit NewCoffee(msg.sender, block.timestamp, _name);
+    }
+}
